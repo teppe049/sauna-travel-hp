798dc03a11e89fb778f35774b93de7253b98bece
Teppei <te10ss04@icloid.com>
2024-09-11 00:39:38 +0900

    ヘッダーのアイコンを押下でスクロールトップできるようにした

--- sauna-travel-hp/src/app/components/base/Header/Header.tsx
+++ sauna-travel-hp/src/app/components/base/Header/Header.tsx
@@ -1,16 +1,24 @@
+"use client";
 import Image from 'next/image';
+
+const backToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
 
 export default function Header() {
     return (
         <header className="pl-4 py-2 fixed w-full bg-white shadow-sm z-50">
-            <a href="/" >
+            <p onClick={backToTop} >
                 <Image src="/header.png"
                     alt="SaunaTravel"
                     width={150}
                     height={100}
                     priority
                 />
-            </a>
+            </p>
         </header>
     )
 
