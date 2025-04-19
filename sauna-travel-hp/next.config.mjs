29b8f9e0945e83753a70e781787793e919f4ee58
Teppei <te10ss04@icloud.com>
2025-04-19 17:14:27 +0900

    fix: リダイレクトするように変更

--- sauna-travel-hp/next.config.mjs
+++ sauna-travel-hp/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.saunatravel.co.jp',
+                    },
+                ],
+                destination: 'https://saunatravel.co.jp/:path*',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
